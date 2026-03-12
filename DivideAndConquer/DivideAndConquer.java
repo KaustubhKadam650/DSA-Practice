5e0ca98203483ef573e81d3ff35220bffadb4ffe
KaustubhKadam650 <kaustubhkadam650@gmail.com>
2026-03-12 22:42:16 +0530

    solved merge by divideNconquer

--- DivideAndConquer/DivideAndConquer.java
+++ DivideAndConquer/DivideAndConquer.java
@@ -12,17 +12,48 @@ public class DivideAndConquer {
             return;
         }
 
-        int mid= (si+ei)/2;
+        int mid= si+(ei-si)/2;
         mergeSort (arr, si,mid);
         mergeSort (arr,mid+1,ei);
         merge(arr,si,mid,ei);
     }
 
     public static void merge(int arr[],int si,int mid,int ei){
+        int temp[] = new int[ei-si+1];
+        int i= si;
+        int j= mid+1;
+        int k=0;
+
+        while (i<= mid && j<=ei) {
+            if (arr[i]<arr[j]) {
+                temp [k] =arr[i];
+                i++;
+                k++;
+                
+            }else{
+                temp [k]=arr[j];
+                j++;
+                k++;
+            }
+        } 
+            while (i<=mid) {
+                temp[k++]= arr[i++];
+                
+            }
+            while (j<=ei) {
+                temp[k++]= arr[j++];
+                
+            }
+
+            for(k=0,i=si;k<temp.length;k++,i++){
+                arr[i]=temp[k];
+            }
         
     }
     public static void main (String args []){
         int arr[]= {6,3,9,5,2,8};
+        mergeSort(arr, 0, arr.length-1);
+        printArr(arr);
 
     }
     
