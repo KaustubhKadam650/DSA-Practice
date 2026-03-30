1ba27aa2c958d3d08ab519a459fa14b20a2cc8d0
KaustubhKadam650 <kaustubhkadam650@gmail.com>
2026-03-30 23:49:10 +0530

    sorted through insertion sort

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -1,3 +1,30 @@
 public class InsertionSort {
+    public static void insertionSort(int nums[]){
+        for(int i=1;i< nums.length;i++){
+            int curr = nums[i];
+            int prev = i-1;
+
+            while (prev>=0 && nums[prev]>curr) {
+                nums[prev+1]=nums[prev];
+                prev--;
+                
+            }
+
+            nums[prev+1]=curr;
+        }
+    }
+
+    public static void printArr(int nums[]){
+        for(int i=0;i<nums.length;i++){
+             System.out.print(nums[i]+" ");
+        }System.out.println();
+        
+    }
     
+    public static void main(String[] args) {
+        int nums[]=new int[]{3, 6, 2, 1, 8, 7, 4, 5, 3, 1};
+
+        insertionSort(nums);
+        printArr(nums);
+    }
 }
