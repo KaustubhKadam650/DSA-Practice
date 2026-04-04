f46361ff6695d640c5e8674d6cda2fee40845766
KaustubhKadam650 <kaustubhkadam650@gmail.com>
2026-04-04 22:47:07 +0530

    solved no to string problem

--- Recursion/ConvertToString.java
+++ Recursion/ConvertToString.java
@@ -1,3 +1,21 @@
 public class ConvertToString {
+    static String digits []={"zero","one","two","three","four","five","six","seven","eight","nine"};
+
+    public static void printDigits(int num){
+        if (num ==0) {
+            return;
+            
+        }
+
+        int lastDigit =num%10;
+        printDigits(num/10);
+        System.err.print(digits[lastDigit]+" ");
+
+    }
+
+    public static void main(String[] args) {
+        printDigits(1234);
+        System.out.println();
+    }
     
 }
