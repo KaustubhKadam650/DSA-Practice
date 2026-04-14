b1ab760dcf15e8ab1441e99216a49e9303ec1864
KaustubhKadam650 <kaustubhkadam650@gmail.com>
2026-04-14 22:47:22 +0530

    problem of rat in maze

---
+++ Backtracking/RatInMaze.java
@@ -0,0 +1,18 @@
+public class RatInMaze {
+
+    public static boolean solveMaze(int maze[][]){
+        int N = maze.length;
+        int sol[][]= new int[N][N];
+        
+    }
+    public static void main(String[] args) {
+        int maze[][] = {{ 1, 0, 0, 0 },
+        { 1, 1, 0, 1 },
+        { 0, 1, 0, 0 },
+        { 1, 1, 1, 1 }
+        };
+
+    }
+}
+    
+
