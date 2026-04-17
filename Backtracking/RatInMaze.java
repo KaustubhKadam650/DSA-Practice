66a4eefec7b95dcd4e18d98862711bade0c3303f
KaustubhKadam650 <kaustubhkadam650@gmail.com>
2026-04-17 22:45:17 +0530

    complete solution of rat in maze problem

--- Backtracking/RatInMaze.java
+++ Backtracking/RatInMaze.java
@@ -3,6 +3,12 @@ public class RatInMaze {
     public static boolean solveMaze(int maze[][]){
         int N = maze.length;
         int sol[][]= new int[N][N];
+        if (solveMazeUntil(maze, 0, 0, sol) == false) {
+            System.out.print("sol not exist");
+            return false;
+        }
+        printSol(sol);
+        return true;
         
     }
     public static boolean isSafe(int maze[][], int x ,int y){
@@ -12,8 +18,31 @@ public class RatInMaze {
 
     public static boolean solveMazeUntil(int maze[][], int x ,int y, int sol[][]){
         if (x== maze.length-1 && y == maze.length-1 && maze[x][y] ==1) {
-            sol[x][y] == 1;
+            sol[x][y] = 1;
             return true;
+        }
+
+        if(isSafe(maze, x, y)==true){
+            if (sol[x][y]==1) 
+               return false;
+                
+            sol[x][y]= 1;
+            if(solveMazeUntil(maze, x+1, y, sol))
+                return true;
+            if(solveMazeUntil(maze, x, y+1, sol))
+                return true;
+            sol[x][y] = 0;
+            return false;
+        }
+        return false;
+    }
+
+    public static void printSol(int sol[][]){
+        for(int i = 0; i<sol.length;i++){
+            for(int j = 0; j<sol.length;j++){
+                System.out.print(" "+sol[i][j]+ " ");
+            }
+            System.out.println();
         }
     }
     public static void main(String[] args) {
@@ -23,6 +52,7 @@ public class RatInMaze {
         { 1, 1, 1, 1 }
         };
 
+        solveMaze(maze);
     }
 }
     
