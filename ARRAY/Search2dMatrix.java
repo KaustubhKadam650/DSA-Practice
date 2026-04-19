b63f63c699308ccdf08551b07d945e4f594ba7d8
KaustubhKadam650 <kaustubhkadam650@gmail.com>
2026-04-19 11:07:14 +0530

    solved search in 2d matrix

---
+++ ARRAY/Search2dMatrix.java
@@ -0,0 +1,27 @@
+public class Search2dMatrix {
+    public static boolean solution(int matrix[][],int target){
+        int row = 0, col = matrix[0].length-1;
+
+        while (row < matrix.length && col>=0) {
+            if (matrix[row][col] == target) {
+                
+                return true; 
+            }else if (matrix[row][col] < target) {
+                row++;
+            }else col--;
+        }return false;
+    }
+
+    public static void main(String[] args) {
+        int matrix[][]= {{1,4,7,11,15},
+        {2,5,8,12,19},
+        {3,6,9,16,22},
+        {10,13,14,17,24}};
+
+        int target= 9;
+
+       System.out.println( solution(matrix, target));
+     
+    }
+    
+}
