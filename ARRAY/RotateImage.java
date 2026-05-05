def2888cabaeffc45010e826665e4135042e0b73
KaustubhKadam650 <kaustubhkadam650@gmail.com>
2026-05-05 23:01:04 +0530

    rotate image problem solved

--- ARRAY/RotateImage.java
+++ ARRAY/RotateImage.java
@@ -1,7 +1,34 @@
 public class RotateImage{
-    public static void rotateImage
+    public static void rotateImage(int matrix[][]){
+         int n= matrix.length;
+
+        for (int i =0 ; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                int temp = matrix[i][j];
+                matrix[i][j]= matrix[j][i];
+                matrix[j][i]= temp;
+            }
+        }
+
+        for (int i=0;i<n;i++){
+            for(int j =0;j<n/2;j++){
+                int temp =matrix[i][j];
+                matrix[i][j]=matrix[i][n-1-j];
+                matrix[i][n-1-j]=temp; 
+            }
+        }
+    }
+    public static void printSol(int matrix[][]){
+        for(int i = 0; i<matrix.length;i++){
+            for(int j = 0; j<matrix.length;j++){
+                System.out.print(" "+matrix[i][j]+ " ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
-        int arr[][] ={{},{},{},{},{},{}} 
-        
+        int matrix[][] ={{1,2,3},{4,5,6},{7,8,9}};
+        rotateImage(matrix);
+        printSol(matrix);
     }
 }
