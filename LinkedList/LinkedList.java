6ff3a6c5521bb59a7db4279a2936cce16c70bd9d
KaustubhKadam650 <kaustubhkadam650@gmail.com>
2026-05-06 20:34:26 +0530

    solved linked list ,remove last and first

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -10,12 +10,13 @@ public class LinkedList {
     }
     public static Node head;
     public static Node tail;
+    public static int size;                    //counting size of ll
 
     //Add First
     public void addFirst(int data){                  //time comple.=O(1)
         //step1= create new node
         Node newNode= new Node(data);
-
+        size++;
         if (head== null) {
             head=tail=newNode;
             return ;
@@ -28,7 +29,7 @@ public class LinkedList {
 
     public void addLast(int data){
         Node newNode = new Node(data);
-
+        size++;
         if(head== null){
             head = tail= newNode;
             return;
@@ -50,6 +51,63 @@ public class LinkedList {
         }
         System.out.println("null");
     }
+
+    public void addMiddle(int idx,int data){
+        if(idx==0){
+            addFirst(data);
+            return;
+        }
+        Node newNode= new Node(data); 
+        size++;
+        Node temp = head;
+        int i=0;
+         
+        while(i< idx-1){
+            temp= temp.next;
+            i++;
+        }
+
+        newNode.next=temp.next;
+        temp.next= newNode;
+    }
+
+    public int removeFirst(){
+        if (size==0) {
+            System.out.println("empty");
+            return Integer.MIN_VALUE;
+        }else if (size==1) {
+            int val= head.data;
+            head=tail=null;
+            size=0;
+            return val;
+        }
+        int val= head.data;
+        head=head.next;
+        size--;
+        return val;
+    }
+
+    public int  removeLast(){
+        if (size==0) {
+            System.out.println("empty");
+            return Integer.MIN_VALUE;
+        }else if (size==1) {
+            int val= head.data;
+            head=tail=null;
+            size=0;
+            return val;
+        }
+        Node prev = head;
+        for(int i =0;i<size-2;i++){
+            prev=prev.next;
+        }
+        int val=prev.next.data;
+        prev.next = null;
+        tail=prev;
+        size--;
+        return val;
+
+    }
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
 
@@ -57,10 +115,17 @@ public class LinkedList {
         ll.print();
         ll.addFirst(1);
         ll.print();
-        ll.addLast(3);
-        ll.print();
         ll.addLast(4);
         ll.print();
+        ll.addLast(5);
+        ll.print();
+        ll.addMiddle(2, 3);
+        ll.print();
+        ll.removeFirst();
+        ll.print();
+        ll.removeLast();
+        ll.print();
+        System.out.println(size);
     }
     
 }
