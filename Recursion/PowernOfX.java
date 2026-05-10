165a54e0de4293d8c865022c645c6feabbbf890a
KaustubhKadam650 <kaustubhkadam650@gmail.com>
2026-05-10 23:53:42 +0530

    updating

--- Recursion/PowernOfX.java
+++ Recursion/PowernOfX.java
@@ -2,11 +2,12 @@
 
 public class PowernOfX {
     public static int power(int x ,int n){
+        //base case
         if (n==0) {
             return 1;
             
         }
-
+        //recursion
         int halfPower = power(x,n/2);
         int halfPowerSq = halfPower * halfPower;
 
