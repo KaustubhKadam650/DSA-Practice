01bae847e580ec826707720929dd869cde2e128d
KaustubhKadam650 <kaustubhkadam650@gmail.com>
2026-05-17 23:47:18 +0530

    now it will work on neg pow also,solved

--- Recursion/PowernOfX.java
+++ Recursion/PowernOfX.java
@@ -1,26 +1,48 @@
 
 
 public class PowernOfX {
-    public static int power(int x ,int n){
-        //base case
-        if (n == 0) {
-            return 1;
+    // public static int power(int x ,int n){
+    //     //base case
+    //     if (n == 0) {
+    //         return 1;
             
-        }
-        //  recursion
-        int halfPower = power(x,n/2);
-        int halfPowerSq = halfPower * halfPower;
+    //     }
+    //     //  recursion
+    //     int halfPower = power(x,n/2);
+    //     int halfPowerSq = halfPower * halfPower;
 
-        if (n%2 !=0) {
-            halfPowerSq = x * halfPowerSq;
+    //     if (n%2 !=0) {
+    //         halfPowerSq = x * halfPowerSq;
+    //     }
+    //     return halfPowerSq;
+
+        public static double power(double x, int n) {
+        double ans = 1.0;
+        long nn = n ;
+        if(nn==0){return 1;}
+
+        if(nn<0){
+            nn = -1* nn;
         }
-        return halfPowerSq;
+       
+       while(nn> 0){
+        if( nn%2 == 1){
+            ans = ans * x;
+            nn = nn -1 ;
+        }else{
+            x = x * x;
+            nn = nn /2;
+        }
+       }
+    if(n < 0) ans = (double)(1.0)/ (double)(ans);
+    return ans;
+
 
     }
      public static void main(String[] args) {
-        int x = 2 ;
+        double x = 2 ;
         int n = -2 ;
 
-        System.out.println("Answer is   = "+power(x, n));
+        System.out.println("Answer is = " + power(x, n));
      }
 }
