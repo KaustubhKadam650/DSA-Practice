a1a43210b19dd5435f721d637b91417781eacd18
KaustubhKadam650 <kaustubhkadam650@gmail.com>
2026-05-24 21:48:38 +0530

    solved

--- LinkedList/MergeTwoList.java
+++ LinkedList/MergeTwoList.java
@@ -1,35 +1,64 @@
 
 import java.util.*;
+
 class ListNode {
     int val;
     ListNode next;
-    ListNode(int val) { this.val = val; }
+
+    ListNode(int val) {
+        this.val = val;
+    }
 }
+
 public class MergeTwoList {
-        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if (list1 != null && list2 !=null){
-            if (list1.val < list2.val){
-                list1.next = mergeTwoLists(list1.next,list2);
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        if (list1 != null && list2 != null) {
+
+            if (list1.val < list2.val) {
+                list1.next = mergeTwoLists(list1.next, list2);
                 return list1;
 
-            }else{
-                list2.next = mergeTwoLists(list1,list2.next);
+            } else {
+                list2.next = mergeTwoLists(list1, list2.next);
                 return list2;
             }
         }
-        if (list1 == null) return list2;
+
+        if (list1 == null)
+            return list2;
 
         return list1;
     }
-    
-        public static void main(String[] args) {
-        ListNode head1 = new Node(5);
-        head1.next = new Node(10);
-        head1.next.next = new Node(15);
-        head1.next.next.next = new Node(40);
 
-       ListNode head2 = new Node(2);
-        head2.next = new Node(3);
-        head2.next.next = new Node(20);
+    // Function to print linked list
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        // First sorted list: 1 -> 3 -> 5
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(3);
+        list1.next.next = new ListNode(5);
+
+        // Second sorted list: 2 -> 4 -> 6
+        ListNode list2 = new ListNode(2);
+        list2.next = new ListNode(4);
+        list2.next.next = new ListNode(6);
+
+        MergeTwoList obj = new MergeTwoList();
+
+        // Merge lists
+        ListNode result = obj.mergeTwoLists(list1, list2);
+
+        // Print merged list
+        System.out.println("Merged List:");
+        printList(result);
     }
 }
