2c8cfc11f5dab0af7d63a54dfaf66c82a4f47fba
KaustubhKadam650 <kaustubhkadam650@gmail.com>
2026-06-05 23:12:12 +0530

    solved but got a bit less understand

--- ARRAY/BestPriceToBuyAndSellStock.java
+++ ARRAY/BestPriceToBuyAndSellStock.java
@@ -1,4 +1,24 @@
 public class BestPriceToBuyAndSellStock {
     
+        public static int maxProfit(int[] prices) {
+        if(prices == null || prices.length==0){
+            return 0;
+        }
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+
+        for(int i = 0; i< prices.length; i++){
+            if(prices[i] < minprice){
+                minprice = prices[i];
+            }else if(prices[i] - minprice > maxprofit){
+                maxprofit = prices[i] - minprice;
+            }
+        }return maxprofit;
+    }
     
+    public static void main(String[] args) {
+        int [ ] prices ={7,1,3,5,6,4};
+
+        System.out.println(maxProfit(prices));
+    }
 }
