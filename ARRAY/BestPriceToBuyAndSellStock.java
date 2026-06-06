76e6795c6f500a5a369021a609e695e2f9937384
KaustubhKadam650 <kaustubhkadam650@gmail.com>
2026-06-06 23:16:57 +0530

    solved

--- ARRAY/BestPriceToBuyAndSellStock.java
+++ ARRAY/BestPriceToBuyAndSellStock.java
@@ -17,7 +17,7 @@ public class BestPriceToBuyAndSellStock {
     }
     
     public static void main(String[] args) {
-        int [ ] prices ={7,1,3,5,6,4};
+        int [] prices ={7,1,3,5,6,4};
 
         System.out.println(maxProfit(prices));
     }
