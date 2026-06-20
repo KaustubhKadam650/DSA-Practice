e77191137f41e8cc37b5969d100adf8189b52810
KaustubhKadam650 <kaustubhkadam650@gmail.com>
2026-06-20 23:31:19 +0530

    solving

--- Greedy algorithm/ActivitySelection.java
+++ Greedy algorithm/ActivitySelection.java
@@ -1,5 +1,11 @@
-package Greedy algorithm;
 
-public  add . {
+
+public class ActivitySelection {
+    public static void main(String[] args) {
+        int start [] ={1,3,0,5,8,5};
+        int end [] ={2,4,6,7,9,9};
+
+        
+    }
     
-}
+} 
