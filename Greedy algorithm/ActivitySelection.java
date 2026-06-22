a40f0295bd8f2181ac37634472a235a032da414b
KaustubhKadam650 <kaustubhkadam650@gmail.com>
2026-06-22 23:06:12 +0530

    solving

--- Greedy algorithm/ActivitySelection.java
+++ Greedy algorithm/ActivitySelection.java
@@ -5,7 +5,15 @@ public class ActivitySelection {
         int start [] ={1,3,0,5,8,5};
         int end [] ={2,4,6,7,9,9};
 
-        for(int i=)
+        //sorting
+        int activities [][] = new int [start.length][3]; 
+        for(int i= 0; i< start.length; i++){
+            activities[i][0] = i;
+            activities[i][1] = start[i];
+            activities[i][2] = end[i];
+        }
+
+        
     }
     
 } 
