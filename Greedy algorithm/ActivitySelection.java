2cdcf81613385870375b62a08480914361643aca
KaustubhKadam650 <kaustubhkadam650@gmail.com>
2026-06-24 23:11:49 +0530

    solved, completed

--- Greedy algorithm/ActivitySelection.java
+++ Greedy algorithm/ActivitySelection.java
@@ -17,6 +17,27 @@ public class ActivitySelection {
         Arrays.sort(activities, Comparator.comparingDouble(o -> o[2]));
 
         //end time basis sort
+        int maxAct= 0;
+        ArrayList<Integer> ans = new ArrayList<>() ;
+
+        maxAct =1;
+        ans.add(activities[0][0]);
+        int lastEnd = activities[0][2];
+        for(int i=1;i<end.length ; i++){
+            if (activities[i][1] >= lastEnd) {
+                //activity select
+                maxAct++;
+                ans.add(activities[i][0]);
+                lastEnd = activities[i][2];
+            }
+        }
+
+        System.out.println("max activities ="+maxAct);
+        for(int i =0; i<ans.size();i++){
+            System.out.print("A"+ans.get(i)+" ");
+
+        }
+        System.out.println();
     }
     
 } 
